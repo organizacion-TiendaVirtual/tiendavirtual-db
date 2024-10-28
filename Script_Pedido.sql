7498c54f2423021b041f7a4804ff12d615ab7978
Johan Patiño Fernandez <patinofernandezjohanbladimir@gmail.com>
2024-10-28 18:37:22 -0500

    Se crea el procedimiento almacenado para la tabla Pedido

--- Script_Pedido.sql
+++ Script_Pedido.sql
@@ -1,40 +1,70 @@
+-- TABLA PEDIDO CRUD
+
 -- Insertar pedido
 DELIMITER //
-CREATE PROCEDURE proInsertOrder(IN v_fecha TEXT,IN v_estado VARCHAR(45),IN v_total INT,IN v_cliente_id INT)
+CREATE PROCEDURE tienInsertOrder(
+IN v_ped_fecha VARCHAR(45),
+IN v_ped_estado VARCHAR(45),
+IN v_ped_total INT,
+IN v_tbl_cliente_clien_id INT
+)
 BEGIN
-INSERT INTO tbl_pedidos(ped_fecha, ped_estado, ped_total, tbl_clientes_cli_id)
-VALUES (v_fecha, v_estado, v_total, v_cliente_id);
+INSERT INTO tbl_pedidos (ped_fecha, ped_estado, ped_total, tbl_cliente_clien_id)
+VALUES (v_ped_fecha, v_ped_estado, v_ped_total, v_tbl_cliente_clien_id);
 END //
 DELIMITER ;
 
 
 -- SELECT PEDIDO
 DELIMITER //
-CREATE PROCEDURE proSelectOrder()
+CREATE PROCEDURE tienSelectOrder()
 BEGIN
     SELECT * FROM tbl_pedidos;
 END //
 DELIMITER ;
 
 
+DELIMITER //
+CREATE PROCEDURE tienSelectOrderID (IN v_ped_id INT)
+BEGIN
+    SELECT * FROM tbl_pedidos WHERE ped_id = v_ped_id;
+END //
+DELIMITER ;
+
+
+
 -- ACTUALIZAR PEDIDO
 DELIMITER //
-CREATE PROCEDURE proUpdateOrder(IN v_id INT, IN v_fecha TEXT, IN v_estado VARCHAR(45), IN v_total INT, IN v_cliente_id INT)
+CREATE PROCEDURE tienUpdateOrder(
+IN v_ped_id INT,
+IN v_ped_fecha VARCHAR(30),
+IN v_ped_estado VARCHAR(45),
+IN v_ped_total DECIMAL (10,2),
+IN v_tbl_cliente_clien_id INT
+)
 BEGIN
 UPDATE tbl_pedidos
-SET 
-ped_fecha = v_fecha,
-ped_estado = v_estado,
-ped_total = v_total,
-tbl_clientes_cli_id = v_cliente_id
-WHERE ped_id = v_id;
+SET ped_fecha = v_ped_fecha,
+ped_estado = v_ped_estado,
+ped_total = v_ped_total,
+tbl_cliente_clien_id = v_tbl_cliente_clien_id
+WHERE ped_id = v_ped_id;
 END //
 DELIMITER ;
 
 -- ELIMINAR PEDIDO 
 DELIMITER //
-CREATE PROCEDURE proDeleteOrder(IN v_id INT)
+CREATE PROCEDURE tienDeleteOrder (IN v_ped_id INT)
 BEGIN
     DELETE FROM tbl_pedidos WHERE ped_id = v_ped_id;
+END //
+DELIMITER ;
+
+-- procedimiento ddl
+DELIMITER //
+CREATE PROCEDURE spSelectPedidosDDL()
+BEGIN
+  SELECT ped_id, CONCAT('Cliente ', tbl_cliente_clien_id, ', Fecha: ', ped_fecha, ', Estado: ', ped_estado, ', Total: ', ped_total) AS DetallePedido
+  FROM tbl_pedidos;
 END //
 DELIMITER ;
